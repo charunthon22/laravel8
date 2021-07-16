f8cada33019563fc0228fc88ae0e0bea486f6f51
charunthon22 <86648729+charunthon22@users.noreply.github.com>
2021-07-16 12:22:06 +0700

    push to heroku

---
+++ resources/views/newgalleryant.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>gallery of ชลันธร</h2>
+<p1>ant<p1>
+<a href="http://localhost/laravel8/public/newgalleryant"> <img src="https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg" width="250" height="200"></a>
+</body>
+</html>
--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h2>gallery of Chvalit ชลันธร</h2>
-<p1>ant<p1>
-<a href=”https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg”><img src="https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg" width="250" height="200"></a>
-<p1>bird<p1>
-<a href="https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg"><img src="https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg" width="250" height="200"></a>
-<p1>cat<p1>
-<a href=”https://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg”><img src="http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg" width="250" height="200"></a>
-<p1>god<p1>
-<a href=”https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg”><img src="https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg" width="250" height="200"></a>
-<p1>spider<p1>
-<a href=”https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg”><img src="https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg" width="250" height="200"></a>
+    <h2>gallery of Chvalit ชลันธร</h2>
+    <p>ant</p>
+    <a href="newgallery/ant"><img src="https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg" width="250" height="200"></a>
+    <p>bird</p>
+    <a href="https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg"><img src="https://www.hebergementwebs.com/image/cc/cc8811773d2cdbeb4d46e5550fc455fe.jpg/falcon-and-the-winter-soldier-falcon-minifigure-captain-america.jpg" width="250" height="200"></a>
+    <p>cat</p>
+    <a href=”https://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg”><img src="http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg" width="250" height="200"></a>
+    <p>god</p>
+    <a href=”https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg”><img src="https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg" width="250" height="200"></a>
+    <p>spider</p>
+    <a href=”https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg”><img src="https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg" width="250" height="200"></a>
 </body>
+
 </html>
